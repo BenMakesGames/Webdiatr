8c2c8b5d8144575a60b1aa11d6322f91177b5148
Ben H <ben@benmakesgames.com>
2025-03-07 21:38:29 -0500

    added docblocks

--- BenMakesGames.Webdiatr/ExceptionMappingExtensions.cs
+++ BenMakesGames.Webdiatr/ExceptionMappingExtensions.cs
@@ -3,11 +3,23 @@ using Microsoft.AspNetCore.Http;
 
 namespace BenMakesGames.Webdiatr;
 
+/// <summary>
+/// Provides extension methods for mapping exceptions to HTTP responses.
+/// Allows for easy configuration of exception handling middleware that converts exceptions to appropriate HTTP results.
+/// </summary>
 public static class ExceptionMappingExtensions
 {
     private static readonly Dictionary<Type, Func<Exception, IResult>> ExceptionHandlers = new();
     private static bool MiddlewareRegistered;
 
+    /// <summary>
+    /// Maps an exception type to a specific HTTP response handler.
+    /// When an exception of the specified type (or a derived type) is thrown, the handler will be invoked to generate the HTTP response.
+    /// </summary>
+    /// <typeparam name="TException">The type of exception to handle</typeparam>
+    /// <param name="app">The WebApplication to add the exception mapping to</param>
+    /// <param name="handler">A function that takes the exception and returns an IResult representing the HTTP response</param>
+    /// <returns>The WebApplication for method chaining</returns>
     public static WebApplication MapException<TException>(this WebApplication app, Func<TException, IResult> handler) where TException : Exception
     {
         if (!MiddlewareRegistered)
@@ -20,6 +32,11 @@ public static class ExceptionMappingExtensions
         return app;
     }
 
+    /// <summary>
+    /// Configures the exception handling middleware that processes mapped exceptions.
+    /// This is automatically called by MapException when the first exception mapping is registered.
+    /// </summary>
+    /// <param name="app">The WebApplication to configure the middleware for</param>
     private static void UseExceptionMapping(WebApplication app)
     {
         app.Use(async (context, next) =>
--- BenMakesGames.Webdiatr/RequestMappingExtensions.cs
+++ BenMakesGames.Webdiatr/RequestMappingExtensions.cs
@@ -5,8 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BenMakesGames.Webdiatr;
 
+/// <summary>
+/// Provides extension methods for mapping MediatR requests to HTTP endpoints without controllers.
+/// </summary>
 public static class RequestMappingExtensions
 {
+    /// <summary>
+    /// Maps a GET endpoint to a MediatR query that returns a response.
+    /// Query parameters are automatically bound from the URL query string to the request object properties.
+    /// </summary>
+    /// <typeparam name="TRequest">The MediatR request type that implements IRequest{TResponse}</typeparam>
+    /// <typeparam name="TResponse">The response type that will be returned from the handler</typeparam>
+    /// <param name="app">The WebApplication to add the endpoint to</param>
+    /// <param name="path">The URL path to map the endpoint to</param>
+    /// <returns>The WebApplication for method chaining</returns>
     public static WebApplication MapGetToQuery<TRequest, TResponse>(this WebApplication app, string path) where TRequest: IRequest<TResponse>
     {
         app.MapGet(path, (IMediator mediatr, [AsParameters] TRequest request) => mediatr.Send(request));
@@ -14,6 +26,14 @@ public static class RequestMappingExtensions
         return app;
     }
 
+    /// <summary>
+    /// Maps a POST endpoint to a MediatR command that does not return a response.
+    /// The request object is deserialized from the request body.
+    /// </summary>
+    /// <typeparam name="TRequest">The MediatR request type that implements IRequest</typeparam>
+    /// <param name="app">The WebApplication to add the endpoint to</param>
+    /// <param name="path">The URL path to map the endpoint to</param>
+    /// <returns>The WebApplication for method chaining</returns>
     public static WebApplication MapPostToCommand<TRequest>(this WebApplication app, string path) where TRequest: IRequest
     {
         app.MapPost(path, (IMediator mediatr, [FromBody] TRequest request) => mediatr.Send(request));
@@ -21,6 +41,15 @@ public static class RequestMappingExtensions
         return app;
     }
 
+    /// <summary>
+    /// Maps a POST endpoint to a MediatR command that returns a response.
+    /// The request object is deserialized from the request body.
+    /// </summary>
+    /// <typeparam name="TRequest">The MediatR request type that implements IRequest{TResponse}</typeparam>
+    /// <typeparam name="TResponse">The response type that will be returned from the handler</typeparam>
+    /// <param name="app">The WebApplication to add the endpoint to</param>
+    /// <param name="path">The URL path to map the endpoint to</param>
+    /// <returns>The WebApplication for method chaining</returns>
     public static WebApplication MapPostToCommand<TRequest, TResponse>(this WebApplication app, string path) where TRequest: IRequest<TResponse>
     {
         app.MapPost(path, (IMediator mediatr, [FromBody] TRequest request) => mediatr.Send(request));
